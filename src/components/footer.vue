2cb8c276283505c3e23edf73b4badc68d4434a3f
MaydayAshin <949210597@qq.com>
2020-07-07 09:49:05 +0800

    7-7

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,13 +1,20 @@
 <template>
-  <!-- Footer Start -->
-  <footer class="footer">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12" style="margin-left: 450px">
-          ©2020 tyust-信息联盟工作室（IOA，Alliance Of Information）
+    <!-- Footer Start -->
+    <footer class="footer">
+        <div class="container-fluid">
+
+            <div class="row">
+
+                <div class="col-2" style="text-align: right">
+
+                     ©2020tyust-信息联盟工作室
+                </div>
+                <div class="col-2" >
+
+                    京ICP证030173号
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </footer>
-  <!-- end Footer -->
+    </footer>
+    <!-- end Footer -->
 </template>
